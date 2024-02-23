dbefa9400fd67fec4b9c389c2c0a0063cb7335a3
Vikash Chaurasiya <120702683+Pforc@users.noreply.github.com>
2024-02-23 22:34:49 +0530

    Update bot.py
    Signed-off-by: Vikash Chaurasiya  <120702683+Pforc@users.noreply.github.com>

--- bot.py
+++ bot.py
@@ -82,7 +82,7 @@ async def start(client: bot, message: Message):
     approvedbot = await client.get_me()
     button = [
         [InlineKeyboardButton("➕️ Add Me To Your Chat ➕️", url=f"http://t.me/{approvedbot.username}?startgroup=botstart")],
-        [InlineKeyboardButton("➕️ Add Me To Your Channel ➕️", url=f"http://t.me/{approvedbot.username}?channel=botstart")],
+        [InlineKeyboardButton("➕️ Add Me To Your Channel ➕️", url=f"http://t.me/{approvedbot.username}?startchannel=botstart")],
         [InlineKeyboardButton('Updates', url='https://t.me/member_adder_tg')]
     ]
     await client.send_message(chat_id=message.chat.id, text=f"**Hello {message.from_user.mention}!\n\nI am the Auto Approver Join Request Bot. \nJust [Add Me To Your Group Channel](http://t.me/{approvedbot.username}?startgroup=botstart) to get started.**", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
